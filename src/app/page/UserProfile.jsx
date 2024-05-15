ece835ecad4b28c8a6ade6f0d65263c48445fb4f
ShadowLearner <edufirman159@gmail.com>
2024-05-15 12:52:11 +0700

    add: Page Folder

---
+++ src/app/page/UserProfile.jsx
@@ -0,0 +1,12 @@
+const UserProfile = () => {
+    return(
+    <div className="container-fluid">
+        <div className="end-0 d-flex flex-column vh-100 position-sticky">
+            <h1 className="poppins">Profile</h1>
+            <div >
+                <img src="Profile/Account.svg" alt="" />
+            </div>
+        </div>
+    </div>
+    )
+}
